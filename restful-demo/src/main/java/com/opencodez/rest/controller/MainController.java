bdd161bcbe003bb9fa84c43b6874d73ff8daed60
Wael OUNI <wouni@spark.local>
2018-06-25 13:12:58 +0100

    create my remote branch

--- restful-demo/src/main/java/com/opencodez/rest/controller/MainController.java
+++ restful-demo/src/main/java/com/opencodez/rest/controller/MainController.java
@@ -27,43 +27,44 @@ import com.opencodez.rest.model.User;
 @RestController
 public class MainController {
 
-	@Autowired
-	private UserService userService;
+    @Autowired
+    private UserService userService;
 
-	@RequestMapping(value = "/users/list", method = RequestMethod.GET)
-	public ResponseEntity<List<User>> list() {
-		List<User> users = new ArrayList<User>(userService.getMapOfUser().values());
-		return new ResponseEntity<List<User>>(users, HttpStatus.OK);
-	}
+    @RequestMapping(value = "/users/list", method = RequestMethod.GET)
+    public ResponseEntity<List<User>> list() {
+	List<User> users = new ArrayList<>(userService.getMapOfUser().values());
+	return new ResponseEntity<>(users, HttpStatus.OK);
+    }
 
-	@RequestMapping(value = "/users/add", method = RequestMethod.POST, consumes={MediaType.APPLICATION_JSON_VALUE})
-	public void addUser(@RequestBody User user) {
-		userService.addUser(user);
-	}
+    @RequestMapping(value = "/users/add", method = RequestMethod.POST, consumes = { MediaType.APPLICATION_JSON_VALUE })
+    public void addUser(@RequestBody User user) {
+	userService.addUser(user);
+    }
 
-	@RequestMapping(value = "/users/update", method = RequestMethod.PUT, consumes={MediaType.APPLICATION_JSON_VALUE})
-	public void updateUser(@RequestBody User user) {
-		userService.updateUser(user);
-	}
+    @RequestMapping(value = "/users/update", method = RequestMethod.PUT, consumes = {
+	    MediaType.APPLICATION_JSON_VALUE })
+    public void updateUser(@RequestBody User user) {
+	userService.updateUser(user);
+    }
 
-	@SuppressWarnings({ "rawtypes", "unchecked" })
-	@RequestMapping(value = "/users/get/{userid}", method = RequestMethod.GET)
-	public ResponseEntity<?> getUser(@PathVariable(value = "userid") String userid) {
-		User usr = userService.getUser(userid);
-		if (usr == null) {
-			return new ResponseEntity("User with id " + userid + " not found", HttpStatus.NOT_FOUND);
-		}
-		return new ResponseEntity<User>(usr, HttpStatus.OK);
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    @RequestMapping(value = "/users/get/{userid}", method = RequestMethod.GET)
+    public ResponseEntity<?> getUser(@PathVariable(value = "userid") String userid) {
+	User usr = userService.getUser(userid);
+	if (usr == null) {
+	    return new ResponseEntity("User with id " + userid + " not found", HttpStatus.NOT_FOUND);
 	}
+	return new ResponseEntity<User>(usr, HttpStatus.OK);
+    }
 
-	@SuppressWarnings({ "unchecked", "rawtypes" })
-	@RequestMapping(value = "/users/delete/{userid}", method = RequestMethod.DELETE)
-	public ResponseEntity<?> deleteUser(@PathVariable(value = "userid") String userid) {
-		User usr = userService.getUser(userid);
-		if (usr == null) {
-			return new ResponseEntity("User with id " + userid + " not found", HttpStatus.NOT_FOUND);
-		}
-		userService.deleteUser(usr.getId());
-		return new ResponseEntity<User>(HttpStatus.NO_CONTENT);
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    @RequestMapping(value = "/users/delete/{userid}", method = RequestMethod.DELETE)
+    public ResponseEntity<?> deleteUser(@PathVariable(value = "userid") String userid) {
+	User usr = userService.getUser(userid);
+	if (usr == null) {
+	    return new ResponseEntity("User with id " + userid + " not found", HttpStatus.NOT_FOUND);
 	}
+	userService.deleteUser(usr.getId());
+	return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
